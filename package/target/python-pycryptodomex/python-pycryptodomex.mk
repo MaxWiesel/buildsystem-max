264e48b2b9d8911cffa8a1fffce5e1e682780fab
max_10 <max_10@gmx.de>
2024-01-21 20:11:28 +0100

    - python-pycryptodomex: update build

--- package/target/python-pycryptodomex/python-pycryptodomex.mk
+++ package/target/python-pycryptodomex/python-pycryptodomex.mk
@@ -9,7 +9,7 @@ PYTHON_PYCRYPTODOMEX_DIR = pycryptodomex-$(PYTHON_PYCRYPTODOMEX_VERSION)
 PYTHON_PYCRYPTODOMEX_SOURCE = pycryptodomex-$(PYTHON_PYCRYPTODOMEX_VERSION).tar.gz
 PYTHON_PYCRYPTODOMEX_SITE = https://files.pythonhosted.org/packages/40/92/efd675dba957315d705f792b28d900bddc36f39252f6713961b4221ee9af
 
-PYTHON_PYCRYPTODOMEX_DEPENDS = python python-setuptools
+PYTHON_PYCRYPTODOMEX_SETUP_TYPE = setuptools
 
 $(D)/python-pycryptodomex: | bootstrap
 	$(call python-package)
