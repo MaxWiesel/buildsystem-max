43b6f0bf1b46898463ae1605b41fa8719bac651e
max_10 <max_10@gmx.de>
2024-01-21 20:14:18 +0100

    - python-twisted: update build

--- package/target/python-twisted/python-twisted.mk
+++ package/target/python-twisted/python-twisted.mk
@@ -9,7 +9,9 @@ PYTHON_TWISTED_DIR = Twisted-$(PYTHON_TWISTED_VERSION)
 PYTHON_TWISTED_SOURCE = Twisted-$(PYTHON_TWISTED_VERSION).tar.gz
 PYTHON_TWISTED_SITE = https://files.pythonhosted.org/packages/b2/ce/cbb56597127b1d51905b0cddcc3f314cc769769efc5e9a8a67f4617f7bca
 
-PYTHON_TWISTED_DEPENDS = python python-setuptools python-zope-interface python-constantly python-incremental python-pyopenssl python-service-identity
+PYTHON_TWISTED_SETUP_TYPE = setuptools
+
+PYTHON_TWISTED_DEPENDS = python-zope-interface python-constantly host-python-incremental python-incremental python-pyopenssl python-service-identity
 
 $(D)/python-twisted: | bootstrap
 	$(call python-package)
