589410d6757743ae4f26d19f8aa9fc1a10d7bd00
max_10 <max_10@gmx.de>
2024-01-21 20:14:37 +0100

    - python-websockets: update build

--- package/target/python-websockets/python-websockets.mk
+++ package/target/python-websockets/python-websockets.mk
@@ -9,7 +9,7 @@ PYTHON_WEBSOCKETS_DIR = websockets-$(PYTHON_WEBSOCKETS_VERSION)
 PYTHON_WEBSOCKETS_SOURCE = websockets-$(PYTHON_WEBSOCKETS_VERSION).tar.gz
 PYTHON_WEBSOCKETS_SITE = https://files.pythonhosted.org/packages/85/dc/549a807a53c13fd4a8dac286f117a7a71260defea9ec0c05d6027f2ae273
 
-PYTHON_WEBSOCKETS_DEPENDS = python python-setuptools
+PYTHON_WEBSOCKETS_SETUP_TYPE = setuptools
 
 $(D)/python-websockets: | bootstrap
 	$(call python-package)
