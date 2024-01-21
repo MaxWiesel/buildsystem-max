19c406d19d7eb3cd2b7c48cd066742fdcef58d13
max_10 <max_10@gmx.de>
2024-01-21 20:06:48 +0100

    - python-incremental: update build

--- package/target/python-incremental/python-incremental.mk
+++ package/target/python-incremental/python-incremental.mk
@@ -4,12 +4,17 @@
 #
 ################################################################################
 
-PYTHON_INCREMENTAL_VERSION = 17.5.0
+PYTHON_INCREMENTAL_VERSION = 22.10.0
 PYTHON_INCREMENTAL_DIR = incremental-$(PYTHON_INCREMENTAL_VERSION)
 PYTHON_INCREMENTAL_SOURCE = incremental-$(PYTHON_INCREMENTAL_VERSION).tar.gz
 PYTHON_INCREMENTAL_SITE = https://files.pythonhosted.org/packages/source/i/incremental
 
-PYTHON_INCREMENTAL_DEPENDS = python python-setuptools
+PYTHON_INCREMENTAL_SETUP_TYPE = setuptools
 
 $(D)/python-incremental: | bootstrap
 	$(call python-package)
+
+# -----------------------------------------------------------------------------
+
+$(D)/host-python-incremental: | bootstrap
+	$(call host-python-package)
