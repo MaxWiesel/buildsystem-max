9b621e913ec38a7d5b78dddd647a72438ad7f433
max_10 <max_10@gmx.de>
2024-01-22 00:31:00 +0100

    - python-pypa-build: update url

--- package/target/python-pypa-build/python-pypa-build.mk
+++ package/target/python-pypa-build/python-pypa-build.mk
@@ -7,7 +7,7 @@
 PYTHON_PYPA_BUILD_VERSION = 1.0.3
 PYTHON_PYPA_BUILD_DIR = build-$(PYTHON_PYPA_BUILD_VERSION)
 PYTHON_PYPA_BUILD_SOURCE = build-$(PYTHON_PYPA_BUILD_VERSION).tar.gz
-PYTHON_PYPA_BUILD_SITE = $(call github,pypa,build,refs/tags/$(PYTHON_PYPA_BUILD_VERSION))
+PYTHON_PYPA_BUILD_SITE = https://files.pythonhosted.org/packages/source/b/build
 
 HOST_PYTHON_PYPA_BUILD_SETUP_TYPE = flit-bootstrap
 
