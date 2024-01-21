2d9ac84d1430162b644892e916afdaa3e6072d35
max_10 <max_10@gmx.de>
2024-01-21 20:00:28 +0100

    - python-setuptools: update build

--- package/target/python-setuptools/python-setuptools.mk
+++ package/target/python-setuptools/python-setuptools.mk
@@ -7,9 +7,18 @@
 PYTHON_SETUPTOOLS_VERSION = 68.0.0
 PYTHON_SETUPTOOLS_DIR = setuptools-$(PYTHON_SETUPTOOLS_VERSION)
 PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/dc/98/5f896af066c128669229ff1aa81553ac14cfb3e5e74b6b44594132b8540e
+PYTHON_SETUPTOOLS_SITE = $(call github,pypa,setuptools,refs/tags/v$(PYTHON_SETUPTOOLS_VERSION))
 
-PYTHON_SETUPTOOLS_DEPENDS = python
+PYTHON_SETUPTOOLS_SETUP_TYPE = pep517
+
+PYTHON_SETUPTOOLS_DEPENDS = host-python-wheel
 
 $(D)/python-setuptools: | bootstrap
 	$(call python-package)
+
+# -----------------------------------------------------------------------------
+
+HOST_PYTHON_SETUPTOOLS_DEPENDS = host-python-wheel
+
+$(D)/host-python-setuptools: | bootstrap
+	$(call host-python-package)
