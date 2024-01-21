bd2407bc3ba41bc2691c5ddce0f93ecf3eb0483a
max_10 <max_10@gmx.de>
2024-01-22 00:29:48 +0100

    - python-pip: bump to version 23.3.2; update url

--- package/target/python-pip/python-pip.mk
+++ package/target/python-pip/python-pip.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PIP_VERSION = 22.3.1
+PYTHON_PIP_VERSION = 23.3.2
 PYTHON_PIP_DIR = pip-$(PYTHON_PIP_VERSION)
 PYTHON_PIP_SOURCE = pip-$(PYTHON_PIP_VERSION).tar.gz
-PYTHON_PIP_SITE = $(call github,pypa,pip,refs/tags/$(PYTHON_PIP_VERSION))
+PYTHON_PIP_SITE = https://files.pythonhosted.org/packages/source/p/pip
 
 PYTHON_PIP_SETUP_TYPE = setuptools
 
