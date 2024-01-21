56fd5060cdec05b0b035735c93461041ab58b6d4
max_10 <max_10@gmx.de>
2024-01-21 20:16:21 +0100

    - yt-dlp: update build

--- package/target/yt-dlp/yt-dlp.mk
+++ package/target/yt-dlp/yt-dlp.mk
@@ -10,7 +10,7 @@ YT_DLP_SOURCE = yt-dlp
 YT_DLP_SITE = https://github.com/yt-dlp/yt-dlp/releases/$(YT_DLP_VERSION)/download
 YT_DLP_SITE_METHOD = curl
 
-YT_DLP_DEPENDS = python brotli python-certifi python-websockets python-requests python-mutagen python-pycryptodomex
+YT_DLP_DEPENDS = brotli python-certifi python-websockets python-requests python-mutagen python-pycryptodomex
 
 define YT_DLP_INSTALL
 	$(INSTALL_EXEC) -D $(DL_DIR)/yt-dlp $(TARGET_BIN_DIR)/yt-dlp
