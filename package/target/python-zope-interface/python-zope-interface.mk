2cc5b386d990fb728d4e0714af64e25b536914c5
max_10 <max_10@gmx.de>
2024-01-21 20:15:57 +0100

    - python-zope-interface: update build

--- package/target/python-zope-interface/python-zope-interface.mk
+++ package/target/python-zope-interface/python-zope-interface.mk
@@ -9,7 +9,7 @@ PYTHON_ZOPE_INTERFACE_DIR = zope.interface-$(PYTHON_ZOPE_INTERFACE_VERSION)
 PYTHON_ZOPE_INTERFACE_SOURCE = zope.interface-$(PYTHON_ZOPE_INTERFACE_VERSION).tar.gz
 PYTHON_ZOPE_INTERFACE_SITE = https://pypi.python.org/packages/source/z/zope.interface
 
-PYTHON_ZOPE_INTERFACE_DEPENDS = python python-setuptools
+PYTHON_ZOPE_INTERFACE_SETUP_TYPE = setuptools
 
 $(D)/python-zope-interface: | bootstrap
 	$(call python-package)
