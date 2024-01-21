8f4a3eb1aaa3b5ad7794e30aa3471b95692d4197
max_10 <max_10@gmx.de>
2024-01-21 20:03:30 +0100

    - python-attr: update build

--- package/target/python-attr/python-attr.mk
+++ package/target/python-attr/python-attr.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_ATTR_VERSION = 0.1.0
+PYTHON_ATTR_VERSION = 0.3.2
 PYTHON_ATTR_DIR = attr-$(PYTHON_ATTR_VERSION)
 PYTHON_ATTR_SOURCE = attr-$(PYTHON_ATTR_VERSION).tar.gz
-PYTHON_ATTR_SITE = https://pypi.python.org/packages/source/a/attr
+PYTHON_ATTR_SITE = $(call github,denis-ryzhkov,attr,refs/tags/$(PYTHON_ATTR_VERSION))
 
-PYTHON_ATTR_DEPENDS = python python-setuptools
+PYTHON_ATTR_SETUP_TYPE = setuptools
 
 $(D)/python-attr: | bootstrap
 	$(call python-package)
