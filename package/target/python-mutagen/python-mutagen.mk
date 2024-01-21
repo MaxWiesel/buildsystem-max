0dcd7b4910bb1d3e2320a72cf9afe61e773876b0
max_10 <max_10@gmx.de>
2024-01-22 00:28:30 +0100

    - python-mutagen: bump to version 1.47.0; update url

--- package/target/python-mutagen/python-mutagen.mk
+++ package/target/python-mutagen/python-mutagen.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_MUTAGEN_VERSION = 1.46.0
+PYTHON_MUTAGEN_VERSION = 1.47.0
 PYTHON_MUTAGEN_DIR = mutagen-$(PYTHON_MUTAGEN_VERSION)
 PYTHON_MUTAGEN_SOURCE = mutagen-$(PYTHON_MUTAGEN_VERSION).tar.gz
-PYTHON_MUTAGEN_SITE = https://github.com/quodlibet/mutagen/releases/download/release-$(PYTHON_MUTAGEN_VERSION)
+PYTHON_MUTAGEN_SITE = https://files.pythonhosted.org/packages/source/m/mutagen
 
 PYTHON_MUTAGEN_SETUP_TYPE = setuptools
 
