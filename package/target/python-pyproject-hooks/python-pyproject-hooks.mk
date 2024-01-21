db7595282fc30474ec84f33931f29d5c108979b2
max_10 <max_10@gmx.de>
2024-01-22 00:31:38 +0100

    - python-pyproject-hooks: update url

--- package/target/python-pyproject-hooks/python-pyproject-hooks.mk
+++ package/target/python-pyproject-hooks/python-pyproject-hooks.mk
@@ -7,7 +7,7 @@
 PYTHON_PYPROJECT_HOOKS_VERSION = 1.0.0
 PYTHON_PYPROJECT_HOOKS_DIR = pyproject-hooks-$(PYTHON_PYPROJECT_HOOKS_VERSION)
 PYTHON_PYPROJECT_HOOKS_SOURCE = pyproject_hooks-$(PYTHON_PYPROJECT_HOOKS_VERSION).tar.gz
-PYTHON_PYPROJECT_HOOKS_SITE = $(call github,pypa,pyproject-hooks,refs/tags/v$(PYTHON_PYPROJECT_HOOKS_VERSION))
+PYTHON_PYPROJECT_HOOKS_SITE = https://files.pythonhosted.org/packages/source/p/pyproject_hooks
 
 HOST_PYTHON_PYPROJECT_HOOKS_SETUP_TYPE = flit-bootstrap
 
