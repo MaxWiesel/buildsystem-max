6bf99edd819e2ce86b0b210879207b2d0a00a31f
max_10 <max_10@gmx.de>
2024-01-21 22:57:39 +0100

    - python: sort PYTHON3_DEPENDS and PYTHON3_CONF_OPTS

--- package/target/python3/python3.mk
+++ package/target/python3/python3.mk
@@ -9,7 +9,10 @@ PYTHON3_DIR = Python-$(PYTHON3_VERSION)
 PYTHON3_SOURCE = Python-$(PYTHON3_VERSION).tar.xz
 PYTHON3_SITE = https://www.python.org/ftp/python/$(PYTHON3_VERSION)
 
-PYTHON3_DEPENDS = ncurses zlib openssl libffi expat bzip2
+PYTHON3_DEPENDS = host-python3 libffi ncurses sqlite bzip2 zlib expat openssl
+
+# no cleanup
+PYTHON3_KEEP_BUILD_DIR = YES
 
 PYTHON3_AUTORECONF = YES
 
@@ -23,46 +26,58 @@ PYTHON3_CONF_ENV = \
 	ac_cv_have_long_long_format=yes \
 	ac_cv_file__dev_ptmx=yes \
 	ac_cv_file__dev_ptc=no \
-	ac_cv_working_tzset=yes \
+	ac_cv_working_tzset=yes
+
+# Make python believe we don't have 'hg', so that it doesn't try to
+# communicate over the network during the build.
+PYTHON3_CONF_ENV += \
 	ac_cv_prog_HAS_HG=/bin/false
 
 PYTHON3_CONF_OPTS = \
 	--enable-shared \
-	--enable-optimizations \
-	--without-ensurepip \
-	--without-cxx-main \
 	--with-build-python=$(HOST_PYTHON_BINARY) \
+	--with-expat=system \
+	--with-libmpdec=none \
+	--with-openssl=$(TARGET_LIB_DIR) \
 	--with-system-ffi \
+	--without-cxx-main \
+	--without-ensurepip \
+	--enable-optimizations \
 	--enable-unicodedata \
-	--disable-pydoc \
-	--disable-test-modules \
-	--disable-tk \
-	--disable-nis \
+	--disable-berkeleydb \
+	--disable-codecs-cjk \
 	--disable-idle3 \
 	--disable-lib2to3 \
-	--disable-berkeleydb
+	--disable-nis \
+	--disable-ossaudiodev \
+	--disable-pydoc \
+	--disable-readline \
+	--disable-test-modules \
+	--disable-tk
 
 $(D)/python3: | bootstrap
 	$(call autotools-package)
 
 # ------------------------------------------------------------------------------
 
+HOST_PYTHON3_DEPENDS = host-libffi
+
 HOST_PYTHON3_AUTORECONF = YES
 
 HOST_PYTHON3_CONF_OPTS = \
 	--without-ensurepip \
 	--without-cxx-main \
-	--disable-sqlite3 \
-	--disable-tk \
 	--with-expat=system \
-	--disable-curses \
-	--disable-codecs-cjk \
-	--disable-nis \
 	--enable-unicodedata \
-	--disable-test-modules \
+	--disable-codecs-cjk \
+	--disable-curses \
 	--disable-idle3 \
-	--disable-uuid \
-	--disable-ossaudiodev
+	--disable-nis \
+	--disable-ossaudiodev \
+	--disable-sqlite3 \
+	--disable-test-modules \
+	--disable-tk \
+	--disable-uuid
 
 define HOST_PYTHON3_INSTALL_SYMLINK
 	ln -sf python3 $(HOST_DIR)/bin/python
@@ -72,4 +87,3 @@ HOST_PYTHON3_HOST_FINALIZE_HOOKS += HOST_PYTHON3_INSTALL_SYMLINK
 
 $(D)/host-python3: | bootstrap
 	$(call host-autotools-package)
-
