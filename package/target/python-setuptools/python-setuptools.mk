c4d9f166f0a5d5a7f5c42220c8dc6b2cc1c15ded
max_10 <max_10@gmx.de>
2024-01-22 00:33:52 +0100

    - python-setuptools: bump to version 69.0.3; update url

--- package/target/python-setuptools/python-setuptools.mk
+++ package/target/python-setuptools/python-setuptools.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_VERSION = 68.0.0
+PYTHON_SETUPTOOLS_VERSION = 69.0.3
 PYTHON_SETUPTOOLS_DIR = setuptools-$(PYTHON_SETUPTOOLS_VERSION)
 PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SITE = $(call github,pypa,setuptools,refs/tags/v$(PYTHON_SETUPTOOLS_VERSION))
+PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/source/s/setuptools
 
 PYTHON_SETUPTOOLS_SETUP_TYPE = pep517
 
