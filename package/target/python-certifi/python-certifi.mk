867f441487f48b05a01481d8bb6f430200ca3c26
max_10 <max_10@gmx.de>
2024-01-21 20:04:23 +0100

    - python-certifi: update build

--- package/target/python-certifi/python-certifi.mk
+++ package/target/python-certifi/python-certifi.mk
@@ -9,7 +9,7 @@ PYTHON_CERTIFI_DIR = certifi-$(PYTHON_CERTIFI_VERSION)
 PYTHON_CERTIFI_SOURCE = certifi-$(PYTHON_CERTIFI_VERSION).tar.gz
 PYTHON_CERTIFI_SITE = https://files.pythonhosted.org/packages/98/98/c2ff18671db109c9f10ed27f5ef610ae05b73bd876664139cf95bd1429aa
 
-PYTHON_CERTIFI_DEPENDS = python python-setuptools
+PYTHON_CERTIFI_SETUP_TYPE = setuptools
 
 $(D)/python-certifi: | bootstrap
 	$(call python-package)
