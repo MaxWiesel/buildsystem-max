b6fb006af335c315fc25384da7286de64a2867f5
max_10 <max_10@gmx.de>
2024-01-22 08:41:00 +0100

    - python-pyproject-hooks: fix typo

--- package/target/python-pyproject-hooks/python-pyproject-hooks.mk
+++ package/target/python-pyproject-hooks/python-pyproject-hooks.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 PYTHON_PYPROJECT_HOOKS_VERSION = 1.0.0
-PYTHON_PYPROJECT_HOOKS_DIR = pyproject-hooks-$(PYTHON_PYPROJECT_HOOKS_VERSION)
+PYTHON_PYPROJECT_HOOKS_DIR = pyproject_hooks-$(PYTHON_PYPROJECT_HOOKS_VERSION)
 PYTHON_PYPROJECT_HOOKS_SOURCE = pyproject_hooks-$(PYTHON_PYPROJECT_HOOKS_VERSION).tar.gz
 PYTHON_PYPROJECT_HOOKS_SITE = https://files.pythonhosted.org/packages/source/p/pyproject_hooks
 
