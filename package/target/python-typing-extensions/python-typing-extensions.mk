4a9ce377656cedaf66c04bb7707e3c3188c3f40f
max_10 <max_10@gmx.de>
2024-01-25 23:57:03 +0100

    - add python-typing-extensions

---
+++ package/target/python-typing-extensions/python-typing-extensions.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# python-typing-extensions
+#
+################################################################################
+
+PYTHON_TYPING_EXTENSIONS_VERSION = 4.9.0
+PYTHON_TYPING_EXTENSIONS_DIR = typing_extensions-$(PYTHON_TYPING_EXTENSIONS_VERSION)
+PYTHON_TYPING_EXTENSIONS_SOURCE = typing_extensions-$(PYTHON_TYPING_EXTENSIONS_VERSION).tar.gz
+PYTHON_TYPING_EXTENSIONS_SITE = https://files.pythonhosted.org/packages/source/t/typing_extensions
+
+PYTHON_TYPING_EXTENSIONS_SETUP_TYPE = flit
+
+$(D)/python-typing-extensions: | bootstrap
+	$(call host-python-package)
+
+$(D)/host-python-typing-extensions: | bootstrap
+	$(call host-python-package)
