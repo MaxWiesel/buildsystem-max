e298ae8a99d71841107bb4bd417a881dcd76066d
max_10 <max_10@gmx.de>
2024-01-25 22:33:32 +0100

    - add python-pluggy

---
+++ package/target/python-pluggy/python-pluggy.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# python-pluggy
+#
+################################################################################
+
+PYTHON_PLUGGY_VERSION = 1.3.0
+PYTHON_PLUGGY_DIR = pluggy-$(PYTHON_PLUGGY_VERSION)
+PYTHON_PLUGGY_SOURCE = pluggy-$(PYTHON_PLUGGY_VERSION).tar.gz
+PYTHON_PLUGGY_SITE = https://files.pythonhosted.org/packages/source/p/pluggy
+
+PYTHON_PLUGGY_SETUP_TYPE = setuptools
+
+PYTHON_PLUGGY_DEPENDS = host-python-setuptools-scm
+
+HOST_PYTHON_PLUGGY_DEPENDS = host-python-setuptools-scm
+
+$(D)/python-pluggy: | bootstrap
+	$(call python-package)
+
+$(D)/host-python-pluggy: | bootstrap
+	$(call host-python-package)
