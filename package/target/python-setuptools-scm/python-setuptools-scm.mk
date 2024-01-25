9f3126407f7352e6a08a41da72c95ec019c928d1
max_10 <max_10@gmx.de>
2024-01-25 23:24:17 +0100

    - add python-setuptools-scm

---
+++ package/target/python-setuptools-scm/python-setuptools-scm.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# python-setuptools-scm
+#
+################################################################################
+
+PYTHON_SETUPTOOLS_SCM_VERSION = 7.1.0
+PYTHON_SETUPTOOLS_SCM_DIR = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION)
+PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
+PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/source/s/setuptools_scm
+
+PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = setuptools
+
+HOST_PYTHON_SETUPTOOLS_SCM_DEPENDENCIES = \
+	host-python-packaging \
+	host-python-typing-extensions
+
+$(D)/host-python-setuptools-scm: | bootstrap
+	$(call host-python-package)
