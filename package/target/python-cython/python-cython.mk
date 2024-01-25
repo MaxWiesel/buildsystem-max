e696eeaa3280a293ac7856a96d843bce946863b7
max_10 <max_10@gmx.de>
2024-01-25 22:30:57 +0100

    - add python-cython

---
+++ package/target/python-cython/python-cython.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-cython
+#
+################################################################################
+
+PYTHON_CYTHON_VERSION = 0.29.36
+PYTHON_CYTHON_DIR = Cython-$(PYTHON_CYTHON_VERSION)
+PYTHON_CYTHON_SOURCE = Cython-$(PYTHON_CYTHON_VERSION).tar.gz
+PYTHON_CYTHON_SITE = https://files.pythonhosted.org/packages/source/c/cython
+
+PYTHON_CYTHON_SETUP_TYPE = setuptools
+
+$(D)/host-python-cython: | bootstrap
+	$(call host-python-package)
