665b83a5c095168d895c98fc4e35b456cbad4f64
max_10 <max_10@gmx.de>
2024-01-25 22:35:13 +0100

    - add python-urllib3

---
+++ package/target/python-urllib3/python-urllib3.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# python-urllib3
+#
+################################################################################
+
+PYTHON_URLLIB3_VERSION = 2.0.7
+PYTHON_URLLIB3_DIR = urllib3-$(PYTHON_URLLIB3_VERSION)
+PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
+PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/source/u/urllib3
+
+PYTHON_URLLIB3_SETUP_TYPE = pep517
+
+HOST_PYTHON_URLLIB3_DEPENDS = host-python-hatchling
+
+$(D)/python-urllib3: | bootstrap
+	$(call python-package)
+
+$(D)/host-python-urllib3: | bootstrap
+	$(call host-python-package)
