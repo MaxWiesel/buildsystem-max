c095fc9f3786df3ba46283cb18132c068997a721
max_10 <max_10@gmx.de>
2024-01-25 22:35:43 +0100

    - add python-yt-dlp

---
+++ package/target/python-yt-dlp/python-yt-dlp.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# python-yt-dlp
+#
+################################################################################
+
+PYTHON_YT_DLP_VERSION = 2023.12.30
+PYTHON_YT_DLP_DIR = yt-dlp-$(PYTHON_YT_DLP_VERSION)
+PYTHON_YT_DLP_SOURCE = yt-dlp-$(PYTHON_YT_DLP_VERSION).tar.gz
+PYTHON_YT_DLP_SITE = https://files.pythonhosted.org/packages/source/y/yt-dlp
+
+PYTHON_YT_DLP_DEPENDS = \
+	python-brotli \
+	python-certifi \
+	python-websockets \
+	python-requests \
+	python-mutagen \
+	python-pycryptodomex
+
+PYTHON_YT_DLP_SETUP_TYPE = setuptools
+
+define PYTHON_YT_DLP_TARGET_CLEANUP
+	rm -rf $(addprefix $(TARGET_SHARE_DIR)/,doc fish man zsh)
+endef
+PYTHON_YT_DLP_TARGET_FINALIZE_HOOKS += PYTHON_YT_DLP_TARGET_CLEANUP
+
+$(D)/python-yt-dlp: | bootstrap
+	$(call python-package)
