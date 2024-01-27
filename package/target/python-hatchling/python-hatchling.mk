255e6f59b0a41e917a9798c6d0b141b9687a8b68
max_10 <max_10@gmx.de>
2024-01-27 22:19:25 +0100

    - python-hatchling: update build

--- package/target/python-hatchling/python-hatchling.mk
+++ package/target/python-hatchling/python-hatchling.mk
@@ -11,12 +11,22 @@ PYTHON_HATCHLING_SITE = https://files.pythonhosted.org/packages/source/h/hatchli
 
 PYTHON_HATCHLING_SETUP_TYPE = pep517
 
+PYTHON_HATCHLING_DEPENDS = \
+	python-editables \
+	python-packaging \
+	python-pathspec \
+	python-pluggy \
+	python-trove-classifiers
+
 HOST_PYTHON_HATCHLING_DEPENDS = \
 	host-python-editables \
 	host-python-packaging \
 	host-python-pathspec \
 	host-python-pluggy \
 	host-python-trove-classifiers
+
+$(D)/python-hatchling: | bootstrap
+	$(call python-package)
 
 $(D)/host-python-hatchling: | bootstrap
 	$(call host-python-package)
