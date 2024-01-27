3f82c2746287f5962288057346623cb597883b4a
max_10 <max_10@gmx.de>
2024-01-27 16:50:37 +0100

    - htop: bump to version 3.3.0

--- package/target/htop/htop.mk
+++ package/target/htop/htop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HTOP_VERSION = 3.2.2
+HTOP_VERSION = 3.3.0
 HTOP_DIR = htop-$(HTOP_VERSION)
 HTOP_SOURCE = htop-$(HTOP_VERSION).tar.gz
 HTOP_SITE = $(call github,htop-dev,htop,$(HTOP_VERSION))
@@ -16,6 +16,11 @@ HTOP_AUTORECONF = YES
 HTOP_CONF_ENV = \
 	ac_cv_file__proc_stat=yes \
 	ac_cv_file__proc_meminfo=yes
+
+ # ac_cv_prog_cc_c99 is required for BR2_USE_WCHAR=n because the C99 test
+# provided by autoconf relies on wchar_t.
+HTOP_CONF_ENV += \
+	ac_cv_prog_cc_c99=-std=gnu99
 
 HTOP_CONF_OPTS = \
 	--enable-affinity \
