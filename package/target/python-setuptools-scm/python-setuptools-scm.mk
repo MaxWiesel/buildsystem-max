91316d3f9490365acdafbff8b0f1658d0abc27db
max_10 <max_10@gmx.de>
2024-01-27 16:55:39 +0100

    - python-setuptools-scm: bump to version 8.0.4

--- package/target/python-setuptools-scm/python-setuptools-scm.mk
+++ package/target/python-setuptools-scm/python-setuptools-scm.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_SCM_VERSION = 7.1.0
-PYTHON_SETUPTOOLS_SCM_DIR = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION)
-PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
+PYTHON_SETUPTOOLS_SCM_VERSION = 8.0.4
+PYTHON_SETUPTOOLS_SCM_DIR = setuptools-scm-$(PYTHON_SETUPTOOLS_SCM_VERSION)
+PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools-scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
 PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/source/s/setuptools_scm
 
-PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = setuptools
+PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = flit
 
 HOST_PYTHON_SETUPTOOLS_SCM_DEPENDS = \
 	host-python-packaging \
