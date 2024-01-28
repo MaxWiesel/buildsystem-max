8e02dc0e8d7a1d31061a75e56e9ce598ffda5569
max_10 <max_10@gmx.de>
2024-01-28 23:08:09 +0100

    - python-pyasn1: update url

--- package/target/python-pyasn1/python-pyasn1.mk
+++ package/target/python-pyasn1/python-pyasn1.mk
@@ -7,7 +7,7 @@
 PYTHON_PYASN1_VERSION = 0.3.6
 PYTHON_PYASN1_DIR = pyasn1-$(PYTHON_PYASN1_VERSION)
 PYTHON_PYASN1_SOURCE = pyasn1-$(PYTHON_PYASN1_VERSION).tar.gz
-PYTHON_PYASN1_SITE = https://pypi.python.org/packages/source/p/pyasn1
+PYTHON_PYASN1_SITE = https://files.pythonhosted.org/packages/source/p/pyasn1
 
 PYTHON_PYASN1_SETUP_TYPE = setuptools
 
