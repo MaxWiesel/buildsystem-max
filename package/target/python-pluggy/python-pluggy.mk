10cd63fc28e0572196c0ffd4ca7375e465cdcaee
max_10 <max_10@gmx.de>
2024-01-28 23:07:37 +0100

    - python-pluggy: bump to version 1.4.0

--- package/target/python-pluggy/python-pluggy.mk
+++ package/target/python-pluggy/python-pluggy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PLUGGY_VERSION = 1.3.0
+PYTHON_PLUGGY_VERSION = 1.4.0
 PYTHON_PLUGGY_DIR = pluggy-$(PYTHON_PLUGGY_VERSION)
 PYTHON_PLUGGY_SOURCE = pluggy-$(PYTHON_PLUGGY_VERSION).tar.gz
 PYTHON_PLUGGY_SITE = https://files.pythonhosted.org/packages/source/p/pluggy
