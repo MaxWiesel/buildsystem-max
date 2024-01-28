ab7c114b8bab24c34c2eec1af64843f038489519
max_10 <max_10@gmx.de>
2024-01-28 23:10:25 +0100

    - python-service-identity: update url

--- package/target/python-service-identity/python-service-identity.mk
+++ package/target/python-service-identity/python-service-identity.mk
@@ -7,7 +7,7 @@
 PYTHON_SERVICE_IDENTITY_VERSION = 17.0.0
 PYTHON_SERVICE_IDENTITY_DIR = service_identity-$(PYTHON_SERVICE_IDENTITY_VERSION)
 PYTHON_SERVICE_IDENTITY_SOURCE = service_identity-$(PYTHON_SERVICE_IDENTITY_VERSION).tar.gz
-PYTHON_SERVICE_IDENTITY_SITE = https://pypi.python.org/packages/source/s/service_identity
+PYTHON_SERVICE_IDENTITY_SITE = https://files.pythonhosted.org/packages/source/s/service_identity
 
 PYTHON_SERVICE_IDENTITY_SETUP_TYPE = setuptools
 
