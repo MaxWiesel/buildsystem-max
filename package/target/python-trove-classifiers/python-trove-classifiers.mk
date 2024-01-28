aa5b4943535a251cd035ae270da55c2ac1591480
max_10 <max_10@gmx.de>
2024-01-28 23:12:59 +0100

    - python-trove-classifiers: bump to version 2024.1.8

--- package/target/python-trove-classifiers/python-trove-classifiers.mk
+++ package/target/python-trove-classifiers/python-trove-classifiers.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_TROVE_CLASSIFIERS_VERSION = 2023.10.18
+PYTHON_TROVE_CLASSIFIERS_VERSION = 2024.1.8
 PYTHON_TROVE_CLASSIFIERS_DIR = trove-classifiers-$(PYTHON_TROVE_CLASSIFIERS_VERSION)
 PYTHON_TROVE_CLASSIFIERS_SOURCE = trove-classifiers-$(PYTHON_TROVE_CLASSIFIERS_VERSION).tar.gz
-PYTHON_TROVE_CLASSIFIERS_SITE = https://files.pythonhosted.org/packages/5b/fa/49b6a09e4f389d4d9406d2947a685de1462ffb676ea6e61c50905e27b0f4
+PYTHON_TROVE_CLASSIFIERS_SITE = https://files.pythonhosted.org/packages/source/t/trove-classifiers
 
 PYTHON_TROVE_CLASSIFIERS_SETUP_TYPE = setuptools
 
