f9cfc94b59b0e23228f3d0c1f66d44eeabe6d25b
max_10 <max_10@gmx.de>
2024-01-28 23:08:54 +0100

    - python-pyopenssl: update url

--- package/target/python-pyopenssl/python-pyopenssl.mk
+++ package/target/python-pyopenssl/python-pyopenssl.mk
@@ -7,7 +7,7 @@
 PYTHON_PYOPENSSL_VERSION = 19.1.0
 PYTHON_PYOPENSSL_DIR = pyOpenSSL-$(PYTHON_PYOPENSSL_VERSION)
 PYTHON_PYOPENSSL_SOURCE = pyOpenSSL-$(PYTHON_PYOPENSSL_VERSION).tar.gz
-PYTHON_PYOPENSSL_SITE = https://pypi.python.org/packages/source/p/pyOpenSSL
+PYTHON_PYOPENSSL_SITE = https://files.pythonhosted.org/packages/source/p/pyOpenSSL
 
 PYTHON_PYOPENSSL_SETUP_TYPE = setuptools
 
