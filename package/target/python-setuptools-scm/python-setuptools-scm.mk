497979811cb7139a8d3455734b033c7e6454e6eb
max_10 <max_10@gmx.de>
2024-01-28 23:12:04 +0100

    - python-setuptools-scm:fix url

--- package/target/python-setuptools-scm/python-setuptools-scm.mk
+++ package/target/python-setuptools-scm/python-setuptools-scm.mk
@@ -7,7 +7,7 @@
 PYTHON_SETUPTOOLS_SCM_VERSION = 8.0.4
 PYTHON_SETUPTOOLS_SCM_DIR = setuptools-scm-$(PYTHON_SETUPTOOLS_SCM_VERSION)
 PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools-scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/source/s/setuptools_scm
+PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/source/s/setuptools-scm
 
 PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = flit
 
