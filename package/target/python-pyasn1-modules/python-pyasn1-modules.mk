6f83679a5185a8b9d85e8c101f5677a943fc2578
max_10 <max_10@gmx.de>
2024-01-28 23:08:33 +0100

    - python-pyasn1-modules: update url

--- package/target/python-pyasn1-modules/python-pyasn1-modules.mk
+++ package/target/python-pyasn1-modules/python-pyasn1-modules.mk
@@ -7,7 +7,7 @@
 PYTHON_PYASN1_MODULES_VERSION = 0.1.4
 PYTHON_PYASN1_MODULES_DIR = pyasn1-modules-$(PYTHON_PYASN1_MODULES_VERSION)
 PYTHON_PYASN1_MODULES_SOURCE = pyasn1-modules-$(PYTHON_PYASN1_MODULES_VERSION).tar.gz
-PYTHON_PYASN1_MODULES_SITE = https://pypi.python.org/packages/source/p/pyasn1-modules
+PYTHON_PYASN1_MODULES_SITE = https://files.pythonhosted.org/packages/source/p/pyasn1-modules
 
 PYTHON_PYASN1_MODULES_SETUP_TYPE = setuptools
 
