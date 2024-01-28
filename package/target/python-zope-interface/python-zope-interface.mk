2b013bac1bee00f029b8f8b920364e0c166fb09e
max_10 <max_10@gmx.de>
2024-01-28 23:14:06 +0100

    - python-zope-interface: update url

--- package/target/python-zope-interface/python-zope-interface.mk
+++ package/target/python-zope-interface/python-zope-interface.mk
@@ -7,7 +7,7 @@
 PYTHON_ZOPE_INTERFACE_VERSION = 5.5.2
 PYTHON_ZOPE_INTERFACE_DIR = zope.interface-$(PYTHON_ZOPE_INTERFACE_VERSION)
 PYTHON_ZOPE_INTERFACE_SOURCE = zope.interface-$(PYTHON_ZOPE_INTERFACE_VERSION).tar.gz
-PYTHON_ZOPE_INTERFACE_SITE = https://pypi.python.org/packages/source/z/zope.interface
+PYTHON_ZOPE_INTERFACE_SITE = https://files.pythonhosted.org/packages/source/z/zope.interface
 
 PYTHON_ZOPE_INTERFACE_SETUP_TYPE = setuptools
 
