83392a3a70a8c306620e788502a6fe2699b93428
max_10 <max_10@gmx.de>
2024-02-18 17:24:44 +0100

    - ninja: bump to version 1.11.1

--- package/host/ninja/host-ninja.mk
+++ package/host/ninja/host-ninja.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NINJA_VERSION = 1.10.2
+NINJA_VERSION = 1.11.1
 NINJA_DIR = ninja-$(NINJA_VERSION)
 NINJA_SOURCE = ninja-$(NINJA_VERSION).tar.gz
 NINJA_SITE = $(call github,ninja-build,ninja,v$(NINJA_VERSION))
