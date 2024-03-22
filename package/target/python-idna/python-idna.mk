d06c128cb375576b5382dc63c479ff8fc120e300
max_10 <max_10@gmx.de>
2024-03-22 19:42:05 +0100

    - python: add patch to workaround missing setup.py

--- package/target/python-idna/python-idna.mk
+++ package/target/python-idna/python-idna.mk
@@ -9,7 +9,7 @@ PYTHON_IDNA_DIR = idna-$(PYTHON_IDNA_VERSION)
 PYTHON_IDNA_SOURCE = idna-$(PYTHON_IDNA_VERSION).tar.gz
 PYTHON_IDNA_SITE = https://files.pythonhosted.org/packages/source/i/idna
 
-PYTHON_IDNA_SETUP_TYPE = flit
+PYTHON_IDNA_SETUP_TYPE = setuptools
 
 $(D)/python-idna: | bootstrap
 	$(call python-package)
