3c36ac6b12a791d7b07a2161436bcb8fe0764aca
max_10 <max_10@gmx.de>
2025-09-30 20:46:57 +0200

    - python-yt-dlp: bump to version 2025.2.19

--- package/target/python-yt-dlp/python-yt-dlp.mk
+++ package/target/python-yt-dlp/python-yt-dlp.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_YT_DLP_VERSION = 2023.12.30
-PYTHON_YT_DLP_DIR = yt-dlp-$(PYTHON_YT_DLP_VERSION)
-PYTHON_YT_DLP_SOURCE = yt-dlp-$(PYTHON_YT_DLP_VERSION).tar.gz
+PYTHON_YT_DLP_VERSION = 2025.2.19
+PYTHON_YT_DLP_DIR = yt_dlp-$(PYTHON_YT_DLP_VERSION)
+PYTHON_YT_DLP_SOURCE = yt_dlp-$(PYTHON_YT_DLP_VERSION).tar.gz
 PYTHON_YT_DLP_SITE = https://files.pythonhosted.org/packages/source/y/yt-dlp
 
-PYTHON_YT_DLP_SETUP_TYPE = setuptools
+PYTHON_YT_DLP_SETUP_TYPE = flit
 
 PYTHON_YT_DLP_DEPENDS = \
 	python-brotli \
