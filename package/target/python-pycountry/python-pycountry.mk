873d0dfeb7c0d00f6298318e1d5cac70788944db
max_10 <max_10@gmx.de>
2026-01-21 21:32:21 +0100

    - add python-python-pycountry

---
+++ package/target/python-pycountry/python-pycountry.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# python-pycountry
+#
+################################################################################
+
+PYTHON_PYCOUNTRY_VERSION = 24.6.1
+PYTHON_PYCOUNTRY_DIR = pycountry-$(PYTHON_PYCOUNTRY_VERSION)
+PYTHON_PYCOUNTRY_SOURCE = pycountry-$(PYTHON_PYCOUNTRY_VERSION).tar.gz
+PYTHON_PYCOUNTRY_SITE = $(PYPI_MIRROR)/p/pycountry
+
+PYTHON_PYCOUNTRY_SETUP_TYPE = hatch
+
+PYTHON_PYCOUNTRY_DEPENDENCIES = host-python-poetry-core
+
+$(D)/python-pycountry: | bootstrap
+	$(call python-package)
