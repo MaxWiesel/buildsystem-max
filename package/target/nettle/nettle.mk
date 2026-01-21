f5914a5fd07eb217b2ca47a5b057fc5eaca6b2aa
max_10 <max_10@gmx.de>
2026-01-21 20:58:11 +0100

    - nettle: bump to version 3.10.2

--- package/target/nettle/nettle.mk
+++ package/target/nettle/nettle.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NETTLE_VERSION = 3.5.1
+NETTLE_VERSION = 3.10.2
 NETTLE_DIR = nettle-$(NETTLE_VERSION)
 NETTLE_SOURCE = nettle-$(NETTLE_VERSION).tar.gz
 NETTLE_SITE = https://ftp.gnu.org/gnu/nettle
