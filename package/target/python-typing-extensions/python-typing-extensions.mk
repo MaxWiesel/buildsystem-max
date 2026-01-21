4a4722fd4cff2be183fcc9f2c30d2f0ece7fab3c
max_10 <max_10@gmx.de>
2026-01-21 22:35:35 +0100

    -python-typing-extensions: bump to version 4.15.0

--- package/target/python-typing-extensions/python-typing-extensions.mk
+++ package/target/python-typing-extensions/python-typing-extensions.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_TYPING_EXTENSIONS_VERSION = 4.9.0
+PYTHON_TYPING_EXTENSIONS_VERSION = 4.15.0
 PYTHON_TYPING_EXTENSIONS_DIR = typing_extensions-$(PYTHON_TYPING_EXTENSIONS_VERSION)
 PYTHON_TYPING_EXTENSIONS_SOURCE = typing_extensions-$(PYTHON_TYPING_EXTENSIONS_VERSION).tar.gz
 PYTHON_TYPING_EXTENSIONS_SITE = $(PYPI_MIRROR)/t/typing_extensions
@@ -12,7 +12,7 @@ PYTHON_TYPING_EXTENSIONS_SITE = $(PYPI_MIRROR)/t/typing_extensions
 PYTHON_TYPING_EXTENSIONS_SETUP_TYPE = flit
 
 $(D)/python-typing-extensions: | bootstrap
-	$(call host-python-package)
+	$(call python-package)
 
 # -----------------------------------------------------------------------------
 
