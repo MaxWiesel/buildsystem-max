e9530443a0b7401d41f19f5d49737772ae545d44
max_10 <max_10@gmx.de>
2026-01-21 21:01:06 +0100

    - add python-calver

---
+++ package/target/python-calver/python-calver.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# python-calver
+#
+################################################################################
+
+PYTHON_CALVER_VERSION = 2025.10.20
+PYTHON_CALVER_DIR = calver-$(PYTHON_CALVER_VERSION)
+PYTHON_CALVER_SOURCE = calver-$(PYTHON_CALVER_VERSION).tar.gz
+PYTHON_CALVER_SITE = $(PYPI_MIRROR)/c/calver
+
+PYTHON_CALVER_SETUP_TYPE = setuptools
+
+# -----------------------------------------------------------------------------
+
+$(HD)/host-python-calver: | bootstrap
+	$(call host-python-package)
