3b8e33d6b3c45f7f3d2a1ac3acb5ee45e0bf4b9f
max_10 <max_10@gmx.de>
2026-01-21 23:48:54 +0100

    - python-incremental: bump to version 24.11.0

--- package/target/python-incremental/python-incremental.mk
+++ package/target/python-incremental/python-incremental.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_INCREMENTAL_VERSION = 22.10.0
+PYTHON_INCREMENTAL_VERSION = 24.11.0
 PYTHON_INCREMENTAL_DIR = incremental-$(PYTHON_INCREMENTAL_VERSION)
 PYTHON_INCREMENTAL_SOURCE = incremental-$(PYTHON_INCREMENTAL_VERSION).tar.gz
 PYTHON_INCREMENTAL_SITE = $(PYPI_MIRROR)/i/incremental
 
-PYTHON_INCREMENTAL_SETUP_TYPE = setuptools
+PYTHON_INCREMENTAL_SETUP_TYPE = hatch
 
 $(D)/python-incremental: | bootstrap
 	$(call python-package)
