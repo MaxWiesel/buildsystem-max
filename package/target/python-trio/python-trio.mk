5d63ef3b7b9e55ec639d4bfa389814c648422a4e
max_10 <max_10@gmx.de>
2026-01-21 22:07:08 +0100

    - add python-trio

---
+++ package/target/python-trio/python-trio.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# python-trio
+#
+################################################################################
+
+PYTHON_TRIO_VERSION = 0.30.0
+PYTHON_TRIO_DIR = trio-$(PYTHON_TRIO_VERSION)
+PYTHON_TRIO_SOURCE = trio-$(PYTHON_TRIO_VERSION).tar.gz
+PYTHON_TRIO_SITE = $(PYPI_MIRROR)/t/trio
+
+PYTHON_TRIO_SETUP_TYPE = setuptools
+
+PYTHON_TRIO_DEPENDENCIES = \
+	python-attrs \
+	python-sortedcontainers \
+	python-idna \
+	python-outcome \
+	python-sniffio
+
+$(D)/python-trio: | bootstrap
+	$(call python-package)
