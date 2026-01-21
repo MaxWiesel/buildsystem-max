cd5e2dca19a113605f6fb04833d0b146d34518c5
max_10 <max_10@gmx.de>
2026-01-21 22:42:31 +0100

    -python-urllib3: bump to version 2.5.0

--- package/target/python-urllib3/python-urllib3.mk
+++ package/target/python-urllib3/python-urllib3.mk
@@ -4,23 +4,16 @@
 #
 ################################################################################
 
-PYTHON_URLLIB3_VERSION = 2.1.0
+PYTHON_URLLIB3_VERSION = 2.5.0
 PYTHON_URLLIB3_DIR = urllib3-$(PYTHON_URLLIB3_VERSION)
 PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
 PYTHON_URLLIB3_SITE = $(PYPI_MIRROR)/u/urllib3
 
-PYTHON_URLLIB3_SETUP_TYPE = setuptools
+PYTHON_URLLIB3_SETUP_TYPE = hatch
 
 PYTHON_URLLIB3_DEPENDENCIES = \
-	host-python-hatchling
+	host-python-hatch-vcs \
+	host-python-setuptools-scm
 
 $(D)/python-urllib3: | bootstrap
 	$(call python-package)
-
-# -----------------------------------------------------------------------------
-
-HOST_PYTHON_URLLIB3_DEPENDENCIES = \
-	host-python-hatchling
-
-$(HD)/host-python-urllib3: | bootstrap
-	$(call host-python-package)
