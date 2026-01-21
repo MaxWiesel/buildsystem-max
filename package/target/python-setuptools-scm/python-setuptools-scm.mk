9045511f46af1aadf3284fec4a1a53d3ed22e94f
max_10 <max_10@gmx.de>
2026-01-21 21:58:44 +0100

    -python-setuptools-scm: bump to version 8.3.1

--- package/target/python-setuptools-scm/python-setuptools-scm.mk
+++ package/target/python-setuptools-scm/python-setuptools-scm.mk
@@ -4,18 +4,19 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_SCM_VERSION = 8.0.4
-PYTHON_SETUPTOOLS_SCM_DIR = setuptools-scm-$(PYTHON_SETUPTOOLS_SCM_VERSION)
-PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools-scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
+PYTHON_SETUPTOOLS_SCM_VERSION = 8.3.1
+PYTHON_SETUPTOOLS_SCM_DIR = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION)
+PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
 PYTHON_SETUPTOOLS_SCM_SITE = $(PYPI_MIRROR)/s/setuptools-scm
 
-PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = flit
+PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = pep517
 
 # -----------------------------------------------------------------------------
 
 HOST_PYTHON_SETUPTOOLS_SCM_DEPENDENCIES = \
 	host-python-packaging \
-	host-python-typing-extensions
+	host-python-typing-extensions \
+	host-python-setuptools
 
 $(HD)/host-python-setuptools-scm: | bootstrap
 	$(call host-python-package)
