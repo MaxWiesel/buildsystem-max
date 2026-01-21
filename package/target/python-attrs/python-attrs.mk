ab0322a53ea430eb2edda071fb938664542757a5
max_10 <max_10@gmx.de>
2026-01-21 20:59:31 +0100

    - python-attrs: bump to version 25.4.0

--- package/target/python-attrs/python-attrs.mk
+++ package/target/python-attrs/python-attrs.mk
@@ -4,12 +4,16 @@
 #
 ################################################################################
 
-PYTHON_ATTRS_VERSION = 16.3.0
+PYTHON_ATTRS_VERSION = 25.4.0
 PYTHON_ATTRS_DIR = attrs-$(PYTHON_ATTRS_VERSION)
 PYTHON_ATTRS_SOURCE = attrs-$(PYTHON_ATTRS_VERSION).tar.gz
 PYTHON_ATTRS_SITE = $(PYPI_MIRROR)/a/attrs
 
-PYTHON_ATTRS_SETUP_TYPE = setuptools
+PYTHON_ATTRS_SETUP_TYPE = flit
+
+PYTHON_ATTRS_DEPENDENCIES = \
+	host-python-hatch-fancy-pypi-readme \
+	host-python-hatch-vcs
 
 $(D)/python-attrs: | bootstrap
 	$(call python-package)
