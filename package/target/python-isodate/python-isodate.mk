92a8ba69b0fa764721b3ebf55bcffd1ed0a05af5
max_10 <max_10@gmx.de>
2026-01-21 21:24:10 +0100

    - add python-isodate

---
+++ package/target/python-isodate/python-isodate.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# python-isodate
+#
+################################################################################
+
+PYTHON_ISODATE_VERSION = 0.7.2
+PYTHON_ISODATE_DIR = isodate-$(PYTHON_ISODATE_VERSION)
+PYTHON_ISODATE_SOURCE = isodate-$(PYTHON_ISODATE_VERSION).tar.gz
+PYTHON_ISODATE_SITE = $(PYPI_MIRROR)/i/isodate
+
+PYTHON_ISODATE_SETUP_TYPE = setuptools
+
+PYTHON_ISODATE_DEPENDENCIES = host-python-setuptools-scm
+
+$(D)/python-isodate: | bootstrap
+	$(call python-package)
