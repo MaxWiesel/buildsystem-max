c95cbf4e579bf11ef74e95a26be54d55187aa45d
max_10 <max_10@gmx.de>
2026-01-21 22:08:35 +0100

    -python-trove-classifiers: bump to version 2025.12.1.14

--- package/target/python-trove-classifiers/python-trove-classifiers.mk
+++ package/target/python-trove-classifiers/python-trove-classifiers.mk
@@ -4,14 +4,17 @@
 #
 ################################################################################
 
-PYTHON_TROVE_CLASSIFIERS_VERSION = 2024.1.8
-PYTHON_TROVE_CLASSIFIERS_DIR = trove-classifiers-$(PYTHON_TROVE_CLASSIFIERS_VERSION)
-PYTHON_TROVE_CLASSIFIERS_SOURCE = trove-classifiers-$(PYTHON_TROVE_CLASSIFIERS_VERSION).tar.gz
+PYTHON_TROVE_CLASSIFIERS_VERSION = 2025.12.1.14
+PYTHON_TROVE_CLASSIFIERS_DIR = trove_classifiers-$(PYTHON_TROVE_CLASSIFIERS_VERSION)
+PYTHON_TROVE_CLASSIFIERS_SOURCE = trove_classifiers-$(PYTHON_TROVE_CLASSIFIERS_VERSION).tar.gz
 PYTHON_TROVE_CLASSIFIERS_SITE = $(PYPI_MIRROR)/t/trove-classifiers
 
 PYTHON_TROVE_CLASSIFIERS_SETUP_TYPE = setuptools
 
 # -----------------------------------------------------------------------------
+
+HOST_PYTHON_TROVE_CLASSIFIERS_DEPENDENCIES = \
+	host-python-calver
 
 $(HD)/host-python-trove-classifiers: | bootstrap
 	$(call host-python-package)
