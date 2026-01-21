f9f04e8ef9464f6da54c31d4fb39b515b39fbe6c
max_10 <max_10@gmx.de>
2026-01-21 21:30:05 +0100

    - python-pip: bump to version 25.3

--- package/target/python-pip/python-pip.mk
+++ package/target/python-pip/python-pip.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PIP_VERSION = 23.3.2
+PYTHON_PIP_VERSION = 25.3
 PYTHON_PIP_DIR = pip-$(PYTHON_PIP_VERSION)
 PYTHON_PIP_SOURCE = pip-$(PYTHON_PIP_VERSION).tar.gz
 PYTHON_PIP_SITE = $(PYPI_MIRROR)/p/pip
@@ -12,7 +12,7 @@ PYTHON_PIP_SITE = $(PYPI_MIRROR)/p/pip
 PYTHON_PIP_SETUP_TYPE = flit
 
 define PYTHON_PIP_INSTALL_PIP_CONF
-	$(INSTALL_DATA) $(PKG_FILES_DIR)/pip.conf $(TARGET_sysconfdir)/pip.conf
+	$(INSTALL_DATA) $(PKG_FILES_DIR)/pip.conf $(TARGET_SYSCONF_DIR)/pip.conf
 endef
 PYTHON_PIP_TARGET_FINALIZE_HOOKS += PYTHON_PIP_INSTALL_PIP_CONF
 
