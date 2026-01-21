a0091f52c0d221d350b545bd3d5550ec756a1483
max_10 <max_10@gmx.de>
2026-01-21 22:06:28 +0100

    - add python-streamlink

---
+++ package/target/python-streamlink/python-streamlink.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# python-streamlink
+#
+################################################################################
+
+PYTHON_STREAMLINK_VERSION = 8.0.0
+PYTHON_STREAMLINK_DIR = streamlink-$(PYTHON_STREAMLINK_VERSION)
+PYTHON_STREAMLINK_SOURCE = streamlink-$(PYTHON_STREAMLINK_VERSION).tar.gz
+PYTHON_STREAMLINK_SITE = $(PYPI_MIRROR)/s/streamlink
+
+PYTHON_STREAMLINK_SETUP_TYPE = setuptools
+
+PYTHON_STREAMLINK_DEPENDENCIES = \
+	host-python-wheel \
+	python-certifi \
+	python-isodate \
+	python-lxml \
+	python-pycountry \
+	python-pycryptodome \
+	python-pysocks \
+	python-requests \
+	python-trio \
+	python-trio-websocket \
+	python-urllib3 \
+	python-websocket-client
+
+define PYTHON_STREAMLINK_INSTALL_CONFIG
+	$(INSTALL_DATA) -D $(PKG_FILES_DIR)/config $(TARGET_DIR)/.config/streamlink/config
+endef
+PYTHON_STREAMLINK_TARGET_FINALIZE_HOOKS += PYTHON_STREAMLINK_INSTALL_CONFIG
+
+define PYTHON_STREAMLINK_TARGET_CLEANUP
+	rm -rf $(addprefix $(TARGET_SHARE_DIR)/,man zsh)
+endef
+PYTHON_STREAMLINK_TARGET_FINALIZE_HOOKS += PYTHON_STREAMLINK_TARGET_CLEANUP
+
+$(D)/python-streamlink: | bootstrap
+	$(call python-package)
