76a8be3d097f33bec5689d765889792739698147
max_10 <max_10@gmx.de>
2026-01-21 22:05:41 +0100

    - add python-sortedcontainers

---
+++ package/target/python-sortedcontainers/python-sortedcontainers.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-sortedcontainers
+#
+################################################################################
+
+PYTHON_SORTEDCONTAINERS_VERSION = 2.4.0
+PYTHON_SORTEDCONTAINERS_DIR = sortedcontainers-$(PYTHON_SORTEDCONTAINERS_VERSION)
+PYTHON_SORTEDCONTAINERS_SOURCE = sortedcontainers-$(PYTHON_SORTEDCONTAINERS_VERSION).tar.gz
+PYTHON_SORTEDCONTAINERS_SITE = $(PYPI_MIRROR)/s/sortedcontainers
+
+PYTHON_SORTEDCONTAINERS_SETUP_TYPE = setuptools
+
+$(D)/python-sortedcontainers: | bootstrap
+	$(call python-package)
