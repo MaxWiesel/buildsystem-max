0c88af193429ff1329341de9758018060cc5700e
max_10 <max_10@gmx.de>
2026-01-25 16:03:56 +0100

    - python-service-identity: bump to version 24.2.0

--- package/target/python-service-identity/python-service-identity.mk
+++ package/target/python-service-identity/python-service-identity.mk
@@ -4,14 +4,16 @@
 #
 ################################################################################
 
-PYTHON_SERVICE_IDENTITY_VERSION = 17.0.0
+PYTHON_SERVICE_IDENTITY_VERSION = 24.2.0
 PYTHON_SERVICE_IDENTITY_DIR = service_identity-$(PYTHON_SERVICE_IDENTITY_VERSION)
 PYTHON_SERVICE_IDENTITY_SOURCE = service_identity-$(PYTHON_SERVICE_IDENTITY_VERSION).tar.gz
 PYTHON_SERVICE_IDENTITY_SITE = $(PYPI_MIRROR)/s/service_identity
 
-PYTHON_SERVICE_IDENTITY_SETUP_TYPE = setuptools
+PYTHON_SERVICE_IDENTITY_SETUP_TYPE = hatch
 
 PYTHON_SERVICE_IDENTITY_DEPENDENCIES = \
+	host-python-hatch-vcs \
+	host-python-hatch-fancy-pypi-readme \
 	python-attr \
 	python-attrs \
 	python-pyasn1
