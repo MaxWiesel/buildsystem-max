8788cb9d0bc01602906ecf92e616957f4398403b
max_10 <max_10@gmx.de>
2026-01-25 16:02:59 +0100

    - python-pyasn1: bump to version 0.6.1

--- package/target/python-pyasn1/python-pyasn1.mk
+++ package/target/python-pyasn1/python-pyasn1.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYASN1_VERSION = 0.3.6
+PYTHON_PYASN1_VERSION = 0.6.1
 PYTHON_PYASN1_DIR = pyasn1-$(PYTHON_PYASN1_VERSION)
 PYTHON_PYASN1_SOURCE = pyasn1-$(PYTHON_PYASN1_VERSION).tar.gz
 PYTHON_PYASN1_SITE = $(PYPI_MIRROR)/p/pyasn1
