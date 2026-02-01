8dd2a30dd5ed6e71d0c11d0ad5b5e2bcaa415957
max_10 <max_10@gmx.de>
2026-02-01 18:55:35 +0100

    - openssl: bump to version 1.1.1w

--- package/target/openssl/openssl.mk
+++ package/target/openssl/openssl.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-OPENSSL_VERSION = 1.1.1t
+OPENSSL_VERSION = 1.1.1w
 OPENSSL_DIR = openssl-$(OPENSSL_VERSION)
 OPENSSL_SOURCE = openssl-$(OPENSSL_VERSION).tar.gz
-OPENSSL_SITE = https://www.openssl.org/source/old/1.1.1
+OPENSSL_SITE = https://github.com/openssl/openssl/releases/download/OpenSSL_$(subst .,_,$(OPENSSL_VERSION))
 
 ifeq ($(TARGET_ARCH),arm)
 OPENSSL_TARGET_ARCH = linux-armv4
@@ -46,6 +46,13 @@ define OPENSSL_CONFIGURE_CMDS
 		./Configure $($(PKG)_CONF_OPTS)
 endef
 
+define OPENSSL_PATCH_MAKEFILE
+	$(SED) 's| build_tests||' $(PKG_BUILD_DIR)/Makefile
+	$(SED) 's|^MANDIR=.*|MANDIR=$(REMOVE_mandir)|' $(PKG_BUILD_DIR)/Makefile
+	$(SED) 's|^HTMLDIR=.*|HTMLDIR=$(REMOVE_htmldir)|' $(PKG_BUILD_DIR)/Makefile
+endef
+OPENSSL_POST_CONFIGURE_HOOKS += OPENSSL_PATCH_MAKEFILE
+
 define OPENSSL_MAKE_DEPEND
 	$(CD) $(PKG_BUILD_DIR); \
 		$($(PKG)_MAKE) depend
@@ -61,6 +68,17 @@ define OPENSSL_TARGET_CLEANUP
 	rm -rf $(addprefix $(TARGET_LIB_DIR)/,engines-1.1)
 endef
 OPENSSL_TARGET_CLEANUP_HOOKS += OPENSSL_TARGET_CLEANUP
+
+OPENSSL_SO_ENDING = 1.1
+OPENSSL_COMPATIBILITY_VERSIONS = 0.9.7 0.9.8 1.0.0 1.0.2 1.1.0
+
+define OPENSSL_COMPATIBILITY_LINKS
+	$(foreach v,$(OPENSSL_COMPATIBILITY_VERSIONS),\
+		ln -sf libcrypto.so.$(OPENSSL_SO_ENDING) $(TARGET_LIB_DIR)/libcrypto.so.$(v)$(sep))
+	$(foreach v,$(OPENSSL_COMPATIBILITY_VERSIONS),\
+		ln -sf libssl.so.$(OPENSSL_SO_ENDING) $(TARGET_LIB_DIR)/libssl.so.$(v)$(sep))
+endef
+OPENSSL_TARGET_FINALIZE_HOOKS += OPENSSL_COMPATIBILITY_LINKS
 
 $(D)/openssl: | bootstrap
 	$(call autotools-package)
