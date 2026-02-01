bb442838b81764bd7da117fbf42a6fd98c6380d0
max_10 <max_10@gmx.de>
2026-02-01 18:59:51 +0100

    - waf: bump to version 2.1.9

--- package/target/waf/waf.mk
+++ package/target/waf/waf.mk
@@ -4,20 +4,19 @@
 #
 ################################################################################
 
-WAF_VERSION = 2.0.24
+WAF_VERSION = 2.1.9
 WAF_DIR = waf-$(WAF_VERSION)
-WAF_SOURCE = waf-$(WAF_VERSION)
+WAF_SOURCE = waf-$(WAF_VERSION).tar.bz2
 WAF_SITE = https://waf.io
 
-HOST_WAF_DEPENDENCIES = host-python
+HOST_WAF_DEPENDENCIES = host-python3
 
 HOST_WAF_BINARY = $(HOST_DIR)/bin/waf
 
-define HOST_WAF_INSTALL
-	$(CD) $(DL_DIR); \
-		$(INSTALL_DATA) -D $(DL_DIR)/$(HOST_WAF_SOURCE) $(HOST_WAF_BINARY)
+define HOST_WAF_INSTALL_BINARY
+	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/waf $(HOST_WAF_BINARY)
 endef
-HOST_WAF_INDIVIDUAL_HOOKS += HOST_WAF_INSTALL
+HOST_WAF_INDIVIDUAL_HOOKS += HOST_WAF_INSTALL_BINARY
 
 $(HD)/host-waf: | bootstrap
-	$(call individual-package,$(PKG_NO_EXTRACT) $(PKG_NO_PATCHES))
+	$(call host-individual-package)
