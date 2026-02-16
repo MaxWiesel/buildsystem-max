7403c69c85e7f9113db7f1b212187a8b40c603dc
max_10 <max_10@gmx.de>
2026-02-16 20:24:18 +0100

    - rsync: fix build with gcc >= 15.x

--- package/target/rsync/rsync.mk
+++ package/target/rsync/rsync.mk
@@ -9,6 +9,13 @@ RSYNC_DIR = rsync-$(RSYNC_VERSION)
 RSYNC_SOURCE = rsync-$(RSYNC_VERSION).tar.gz
 RSYNC_SITE = https://download.samba.org/pub/rsync/src
 
+RSYNC_DEPENDENCIES = zlib popt
+
+ifeq ($(GCC_VERSION),$(filter $(GCC_VERSION),15.2.0))
+RSYNC_MAKE_OPTS += \
+	CFLAGS="$(TARGET_CFLAGS) -std=gnu17"
+endif
+
 RSYNC_CONF_OPTS = \
 	--with-included-zlib=no \
 	--with-included-popt=no \
