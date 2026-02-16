3e073cb1eb419fabee91c965d068bacd37ddffc2
max_10 <max_10@gmx.de>
2026-02-16 20:18:09 +0100

    - ntp: bump to version 4.2.8p18

--- package/target/ntp/ntp.mk
+++ package/target/ntp/ntp.mk
@@ -4,10 +4,12 @@
 #
 ################################################################################
 
-NTP_VERSION = 4.2.8p15
+NTP_VERSION = 4.2.8p18
 NTP_DIR = ntp-$(NTP_VERSION)
 NTP_SOURCE = ntp-$(NTP_VERSION).tar.gz
 NTP_SITE = https://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-$(basename $(NTP_VERSION))
+
+NTP_AUTORECONF = YES
 
 NTP_CONF_OPTS = \
 	--docdir=$(REMOVE_docdir) \
