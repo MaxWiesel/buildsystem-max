231137f2fc45aca38f910904bfaaff2a4a16c079
max_10 <max_10@gmx.de>
2026-02-16 20:05:00 +0100

    - gmp: bump to version 6.3.0

--- package/target/gmp/gmp.mk
+++ package/target/gmp/gmp.mk
@@ -4,13 +4,19 @@
 #
 ################################################################################
 
-GMP_VERSION = 6.2.1
+GMP_VERSION = 6.3.0
 GMP_DIR = gmp-$(GMP_VERSION)
 GMP_SOURCE = gmp-$(GMP_VERSION).tar.xz
 GMP_SITE = $(GNU_MIRROR)/gmp
 
+GMP_AUTORECONF = YES
+
+GMP_CONF_ENV = \
+	CC_FOR_BUILD="$(HOSTCC) -std=c99"
+
 GMP_CONF_OPTS = \
-	--enable-silent-rules
+	--disable-assembly \
+	--enable-cxx
 
 $(D)/gmp: | bootstrap
 	$(call autotools-package)
