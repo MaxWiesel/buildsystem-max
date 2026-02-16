4508d67838a9cf8c7db63d8b9f44e88c6c209fe2
max_10 <max_10@gmx.de>
2026-02-16 20:21:41 +0100

    - python-lxml: fix build with gcc >= 14.x

--- package/target/python-lxml/python-lxml.mk
+++ package/target/python-lxml/python-lxml.mk
@@ -13,6 +13,11 @@ PYTHON_LXML_SETUP_TYPE = setuptools
 
 PYTHON_LXML_DEPENDENCIES = host-python-cython libiconv libxml2 libxslt zlib
 
+ifeq ($(GCC_VERSION),$(filter $(GCC_VERSION),14.3.0 15.2.0))
+PYTHON_LXML_PYTHON_ENV = \
+	-Wno-error=implicit-function-declaration
+endif
+
 # python-lxml needs these scripts in order to properly detect libxml2 and
 # libxslt compiler and linker flags
 PYTHON_LXML_BUILD_OPTS = \
