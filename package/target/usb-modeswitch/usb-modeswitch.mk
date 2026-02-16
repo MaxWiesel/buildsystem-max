c941ffccafe1a225d3bb6e48103aefd62d2bc796
max_10 <max_10@gmx.de>
2026-02-16 20:35:22 +0100

    - usb-modeswitch: bump to version 2.6.2

--- package/target/usb-modeswitch/usb-modeswitch.mk
+++ package/target/usb-modeswitch/usb-modeswitch.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-USB_MODESWITCH_VERSION = 2.6.0
+USB_MODESWITCH_VERSION = 2.6.2
 USB_MODESWITCH_DIR = usb-modeswitch-$(USB_MODESWITCH_VERSION)
 USB_MODESWITCH_SOURCE = usb-modeswitch-$(USB_MODESWITCH_VERSION).tar.bz2
 USB_MODESWITCH_SITE = http://www.draisberghof.de/usb_modeswitch
