1f1892bc4e3b866a77174a4677b10e148ee494d7
max_10 <max_10@gmx.de>
2026-02-16 20:17:04 +0100

    - ninja: bump to version 1.13.2

--- package/target/ninja/ninja.mk
+++ package/target/ninja/ninja.mk
@@ -4,10 +4,14 @@
 #
 ################################################################################
 
-NINJA_VERSION = 1.11.1
+NINJA_VERSION = 1.13.2
 NINJA_DIR = ninja-$(NINJA_VERSION)
 NINJA_SOURCE = ninja-$(NINJA_VERSION).tar.gz
 NINJA_SITE = $(call github,ninja-build,ninja,v$(NINJA_VERSION))
+
+# -----------------------------------------------------------------------------
+
+HOST_NINJA_BINARY = $(HOST_DIR)/bin/ninja
 
 $(HD)/host-ninja: | bootstrap
 	$(call host-cmake-package)
