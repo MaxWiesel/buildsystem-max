31b5cf51e4f9893adb9beb963d3da53c2e622a8c
max_10 <max_10@gmx.de>
2026-02-19 11:55:18 +0100

    - wlan-qcom: bump to version 4.5.25.55

--- package/linux/wlan-qcom/wlan-qcom.mk
+++ package/linux/wlan-qcom/wlan-qcom.mk
@@ -4,15 +4,15 @@
 #
 ################################################################################
 
-WLAN_QCOM_VERSION = 4.5.25.46
+WLAN_QCOM_VERSION = 4.5.25.55
 WLAN_QCOM_DIR = qcacld-2.0-$(WLAN_QCOM_VERSION)
 WLAN_QCOM_SOURCE = qcacld-2.0-$(WLAN_QCOM_VERSION).tar.gz
-WLAN_QCOM_SITE = https://www.codeaurora.org/cgit/external/wlan/qcacld-2.0/snapshot
+WLAN_QCOM_SITE = https://source.mynonpublic.com
 
 WLAN_QCOM_DEPENDENCIES = kernel wlan-qcom-firmware wireless-regdb
 
 define WLAN_QCOM_INSTALL_BINARY
-	$(INSTALL_DATA) wlan.ko $(TARGET_MODULES_DIR)/extra/wlan.ko
+	$(INSTALL_DATA) $(PKG_BUILD_DIR)/wlan.ko $(TARGET_MODULES_DIR)/extra/wlan.ko
 	mkdir -p ${TARGET_DIR}/etc/modules-load.d
 	for i in wlan; do \
 		echo $$i >> ${TARGET_DIR}/etc/modules-load.d/wlan.conf; \
