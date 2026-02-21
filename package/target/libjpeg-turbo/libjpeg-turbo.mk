b5ca4697a01b3590121e9c9f30bac1ad26d591f2
max_10 <max_10@gmx.de>
2026-02-21 20:15:18 +0100

    - libjpeg-turbo: bump to version 3.1.3

--- package/target/libjpeg-turbo/libjpeg-turbo.mk
+++ package/target/libjpeg-turbo/libjpeg-turbo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBJPEG_TURBO_VERSION = 3.0.2
+LIBJPEG_TURBO_VERSION = 3.1.3
 LIBJPEG_TURBO_DIR = libjpeg-turbo-$(LIBJPEG_TURBO_VERSION)
 LIBJPEG_TURBO_SOURCE = libjpeg-turbo-$(LIBJPEG_TURBO_VERSION).tar.gz
 LIBJPEG_TURBO_SITE = $(call github,libjpeg-turbo,libjpeg-turbo,refs/tags/$(LIBJPEG_TURBO_VERSION))
@@ -16,6 +16,11 @@ LIBJPEG_TURBO_CONF_OPTS = \
 	-DWITH_JAVA=OFF \
 	-DWITH_TURBOJPEG=OFF \
 	-DWITH_JPEG8=80
+
+define LIBJPEG_TURBO_TARGET_CLEANUP
+	rm -rf $(addprefix $(TARGET_LIB_DIR)/,cmake)
+endef
+LIBJPEG_TURBO_TARGET_FINALIZE_HOOKS += LIBJPEG_TURBO_TARGET_CLEANUP
 
 $(D)/libjpeg-turbo: | bootstrap
 	$(call cmake-package)
